6aaae4e1c15634c2cbbf15630c6fc23271783ca9
Ubuntu <ubuntu@ip-172-31-39-224.ap-south-1.compute.internal>
2026-01-31 10:05:35 +0000

    script added

--- script.sh
+++ script.sh
@@ -1,1 +1,18 @@
-echo "hello world"
+#!/bin/bash
+set -e
+
+IMAGE_NAME=nginx-welcome
+CONTAINER_NAME=nginx-welcome-container
+PORT=8080
+
+echo "Stopping old container (if any)..."
+docker rm -f $CONTAINER_NAME || true
+
+echo "Building Docker image..."
+docker build -t $IMAGE_NAME .
+
+echo "Running container..."
+docker run -d -p $PORT:80 --name $CONTAINER_NAME $IMAGE_NAME
+
+echo "NGINX is live on port $PORT"
+
